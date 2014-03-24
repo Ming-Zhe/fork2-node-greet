f1cdf663ad9000a164301b17b115da7d543ada2d
Ming-Zhe <law.gravitys@gmail.com>
2014-03-24 17:24:00 +0800

    finish

---
+++ bin/greet.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+var greet = require("greet");
+
+// console.log(process.argv[2]);
+console.log(greet(process.argv[2],process.argv[3]));
--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 // // is the the same as requiring
 // var greet = require("/greet_module_path/index.js");
 
-module.exports = function greet(name) {
-  return "hello, " + name;
+module.exports = function greet(name,drunk) {
+  if(drunk === '--drunk') {
+    return "hello " + name + ", you look sexy today";
+  } else {
+    return "hello, " + name;
+  }
 }
-
