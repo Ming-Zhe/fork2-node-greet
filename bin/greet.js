ece010acafd07cd7f71cb4f87202a92a56ade885
Ming-Zhe <law.gravitys@gmail.com>
2014-03-24 21:39:41 +0800

    finish

--- bin/greet.js
+++ bin/greet.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 var greet = require("greet");
+var parseArgs = require('minimist')(process.argv.slice(2));
 
-// console.log(process.argv[2]);
-console.log(greet(process.argv[2],process.argv[3]));
+// console.dir(parseArgs);
+// console.log(parseArgs);
+console.log(greet(parseArgs._ , parseArgs.drunk));
--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 // // is the the same as requiring
 // var greet = require("/greet_module_path/index.js");
 
+// var parseArgs = require('minimist');
+
 module.exports = function greet(name,drunk) {
-  if(drunk === '--drunk') {
+  if(drunk ) {
     return "hello " + name + ", you look sexy today";
   } else {
     return "hello, " + name;
