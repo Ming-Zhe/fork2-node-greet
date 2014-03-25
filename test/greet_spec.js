485764994872e25cc3d0f6e477c5f1c686b45489
Ming-Zhe <law.gravitys@gmail.com>
2014-03-25 17:19:37 +0800

    finish

--- test/greet_spec.js
+++ test/greet_spec.js
@@ -1,9 +1,9 @@
 // var expect = require("chai").expect;
 var greet = require("greet");
 
-describe('greet', function(){
+describe('greet', function(){ // Have no idea which function should been used here.
     it("should greet a person by name", function(){
-        expect(greet('name', 'drunk')).to.have.length.above(7);
+        expect(greet('name')).to.have.length.above(7);
     });
     it("should greet a person flirtatiously if drunk", function(){
         expect(greet('name', 'drunk')).to.have.length.above(7);
